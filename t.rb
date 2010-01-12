1650e19e7695c2bc8585b9ecee057ecde0497677
Joe Goggins <goggins@umn.edu>
2010-01-11 18:33:20 -0600

    Sanction-ui tweaks

--- t.rb
+++ t.rb
@@ -19,27 +19,41 @@ Sanction::Role::Definition.names.each do |role_name| # [:super_user, :writer, :v
   #
   Sanction::Role::Definition.with(role_name).each do |role_def|
     role_def.principals.each do |principal_class| # ["User", "ApiUser"]
-      # if role_def.principals.length > 1
-      #         puts "  PRINCIPAL CLASS: #{principal_class.to_s}"
-      #       end
       role_def.permissionables.each do |permissionable_class| # ["Magazine", "User"]
-        # if role_def.permissionables.length > 1
-        #   puts "  PERMISSIONABLE CLASS: #{permissionable_class.to_s}"
-        # end     
-        
-        
+  
         @actual_roles = @roles.find_all {|r|
            r.name.to_sym == role_def.name && 
            r.principal_type.to_s == principal_class &&
            r.permissionable_type.to_s == permissionable_class
         }
-       # pp @actual_roles
         @actual_roles.sort! {|a,b| "#{a.principal_type} #{a.permissionable_type}" <=> "#{b.principal_type} #{b.permissionable_type}"}
+        
+        # Only render the name once
+        rendered_principal_ids = []
         @actual_roles.each do |granular_role|
+          unless rendered_principal_ids.include? granular_role.principal_id
+            if granular_role.principal.blank?
+              puts "  Invalid non-existant principal"
+            else
+              if role_def.principals.length > 1
+                puts "  #{granular_role.principal.name} (#{principal_class.to_s})"
+              else
+                puts "  #{granular_role.principal.name}"
+              end
+            end
+            rendered_principal_ids << granular_role.principal_id
+          end
+          
           if granular_role.permissionable.blank?
-            puts "  Access over all #{granular_role.permissionable_type.to_s.pluralize}"
+            puts "    Access over all #{granular_role.permissionable_type.to_s.pluralize}"
           else
-            puts "  #{granular_role.permissionable.name}"
+            # Indicate the class if there are multiple types of permissionables
+            #
+            if role_def.permissionables.length > 1
+              puts "    #{granular_role.permissionable.name} (#{permissionable_class.to_s})"
+            else
+              puts "    #{granular_role.permissionable.name}"
+            end
           end
         end      
         
