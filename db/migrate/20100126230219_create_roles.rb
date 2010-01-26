97a2332b6c474bb3accebdb3828c252c6c555be2
Joe Goggins <goggins@umn.edu>
2010-01-26 17:17:34 -0600

    String_ids for principals and permissionables in migration now

--- db/migrate/20100126230219_create_roles.rb
+++ db/migrate/20100126230219_create_roles.rb
@@ -1,14 +1,18 @@
 class CreateRoles < ActiveRecord::Migration
   def self.up
     create_table :roles do |t|
-      t.belongs_to :principal, :polymorphic => true
-      t.belongs_to :permissionable, :polymorphic => true
+      t.string     :principal_id, :limit => 32
+      t.string     :principal_type, :limit => 64
+      t.string     :permissionable_id, :limit => 32
+      t.string     :permissionable_type, :limit => 64
       t.string     :name
       t.boolean    :global, :default => false
     end
     
     add_index :roles, [:principal_id, :principal_type]
+    add_index :roles, [:permissionable_id, :permissionable_type]
     add_index :roles, :name
+    add_index :roles, :global
   end
 
   def self.down
