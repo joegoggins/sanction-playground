f0ede096a19e2751fddaff31d43aaed1bc0e0fea
Joe Goggins <goggins@umn.edu>
2010-01-27 16:33:44 -0600

    Unstable...will finish at home

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,11 @@
 class MainController < ApplicationController
   def index
+    if current_user.has? :editor
+      render :text => 'asdf'
+    else
+      redirect_to sanction_ui_access_denied_path(:editor) and return
+      render :text => 'xxx'
+    end
   end
 
 end
